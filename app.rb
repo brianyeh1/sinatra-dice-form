3ffce47f62b34f92af03ac6335b38bee39962318
brianyeh1 <nyeh@chicagobooth.edu>
2025-04-21 00:37:50 +0000

    initial commit

--- app.rb
+++ app.rb
@@ -3,7 +3,21 @@ require "sinatra/reloader"
 
 get("/") do
   "
-  <h1>Welcome to your Sinatra App!</h1>
+  <h1>Dice roll</h1>
   <p>Define some routes in app.rb</p>
   "
+  #<form action="/process_roll">
+  #<label for="dice_id">Number of dice to roll?</label>
+  #<input id="dice_id" name="dice">
+  #<label for="side_id">How many sides per die?</label>
+  #<input id="side_id" name="side">
+  #<button>Roll!</button>
+  #</form>
+end
+
+get("/process_roll") do
+  @num_dice = params.fetch("dice").to_i
+  @num_sides = params.fetch("sides").to_i
+  
+  erb(:results)
 end
